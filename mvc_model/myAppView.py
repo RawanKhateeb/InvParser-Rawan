bf3f46652f51a96f425f2287ee3a5cd320192147
RawanKhateeb <117752616+RawanKhateeb@users.noreply.github.com>
2026-01-20 09:25:38 +0200

    Fix/update myAppView

--- mvc_model/myAppView.py
+++ mvc_model/myAppView.py
@@ -13,6 +13,17 @@ from mvc_model.controller.controller import (
 )
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3002",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)
